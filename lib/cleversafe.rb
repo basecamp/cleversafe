d96cc39814c2898f75cf76601073aa8730cbe8af
Jeffrey Hardy <packagethief@gmail.com>
2013-05-07 20:26:00 -0400

    Monkeypatch RestClient to keep it from closing IO objects after upload

--- lib/cleversafe.rb
+++ lib/cleversafe.rb
@@ -2,3 +2,12 @@ require 'cleversafe/errors'
 require 'cleversafe/vault'
 require 'cleversafe/object'
 require 'cleversafe/connection'
+
+require 'rest-client'
+
+# Monkeypatch RestClient to prevent it from closing IO objects after they're
+# uploaded, which it really shouldn't do.
+class RestClient::Payload::Streamed
+  def close
+  end
+end
